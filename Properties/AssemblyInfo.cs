cb3a29b3e7e55dd19d18c2f88b4e4b65dd5c02d3
tomek51882 <tomek51882@gmail.com>
2020-04-17 13:53:50 +0200

    Create project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Informacje ogólne o zestawie zależą od poniższego
+// zestawu atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// związane z zestawem.
+[assembly: AssemblyTitle("SQLapp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SQLapp")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie dla atrybutu ComVisible wartości false powoduje, że typy w tym zestawie stają się niewidoczne
+// dla składników COM. Jeśli musisz uzyskiwać dostęp do typu w tym zestawie
+// z modelu COM, ustaw dla atrybutu ComVisible tego typu wartość true.
+[assembly: ComVisible(false)]
+
+// Poniższy identyfikator GUID odpowiada atrybutowi ID biblioteki typów typelib, jeśli ten projekt jest uwidaczniany w modelu COM
+[assembly: Guid("178e6d8b-eed1-4e23-b035-527ba8fbd407")]
+
+// Informacje o wersji zestawu obejmują następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie te wartości lub użyć wartości domyślnych numerów kompilacji i poprawki,
+// stosując znak „*”, jak pokazano poniżej:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SQLapp.Startup))]
+namespace SQLapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
