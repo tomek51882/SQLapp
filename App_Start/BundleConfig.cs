29135cb0d147bd41311efa8011fe693d8ab95a55
Tomasz Jędrzejczak <tomek51882@gmail.com>
2020-05-13 12:40:23 +0200

    Changed Bootstrap and Added Task Creator
    Changed Bootstrap from V3 to V4
    Added Task Creator for Admins and Teachers in main page.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace SQLapp
         // Aby uzyskać więcej informacji o grupowaniu, odwiedź stronę https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.ResetAll();
+            bundles.IgnoreList.Clear();
+            BundleTable.EnableOptimizations = true;
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -19,12 +22,14 @@ namespace SQLapp
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
+                        "~/Scripts/umd/popper.js"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
+                        "~/Scripts/bootstrap.js"));
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                        "~/Content/bootstrap.css"));
+            
+            
         }
     }
 }
