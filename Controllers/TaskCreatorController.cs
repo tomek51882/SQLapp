3fb0dc83b444af182d724b0476bae1db8f5d083e
Tomasz Jędrzejczak <tomek51882@gmail.com>
2020-05-14 10:36:47 +0200

    Preparation for TaskCreator's logic

---
+++ Controllers/TaskCreatorController.cs
@@ -0,0 +1,31 @@
+﻿using SQLapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SQLapp.Controllers
+{
+    public class TaskCreatorController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: TaskCreator
+        public ActionResult Index()
+        {
+            int max = 0;
+            try
+            {
+                max = db.Tasks.Max(p => p.Id);
+            }
+            catch (Exception e)
+            {
+                max = 0;
+            }
+
+
+            ViewBag.id = max;
+            return View();
+        }
+    }
+}
